c2b79ce33a57d1b4780901e234e5a899cd694007
Michael Gant <michael.j.g.za@gmail.com>
2019-07-28 11:16:57 +0200

    get_balance and stop_all_orders working

--- src/api.jl
+++ src/api.jl
@@ -1,18 +1,20 @@
+const API_URL = "https://api.mybitx.com/api/1/"
 
-function set_auth_token(token)
+
+function set_auth_token(token="")
     if token == ""
-        auth_token = readline("auth_token.txt")
+        auth_token = readline("src/auth_token.txt")
         ENV["luno_auth_token"] = base64encode(auth_token)
     else
         ENV["luno_auth_token"] = base64encode(token)
     end
 end
 
-function get_auth_token(auth=true)
-    if auth
+function get_auth_token(token="")
+    if get(ENV, "luno_auth_token", false) == false
+        set_auth_token(token)
         return Dict("Authorization" => "Basic " * ENV["luno_auth_token"])
     else
-        set_auth_token()
         return Dict("Authorization" => "Basic " * ENV["luno_auth_token"])
     end
 end
@@ -22,7 +24,7 @@ function get_price(pair="XBTZAR")
     query_dict = Dict("pair" => pair)
     resp = JSON.parse(
         String(
-            HTTP.get("https://api.mybitx.com/api/1/ticker";
+            HTTP.get(API_URL * "ticker";
                 query=query_dict).body))
 
     return Dict(
@@ -33,35 +35,52 @@ function get_price(pair="XBTZAR")
     "rolling_24h_vol" => parse(Float64, resp["rolling_24_hour_volume"]))
 end
 
-function get_balance(auth=true)
+
+function get_balance(token="")
     price_dict = get_price()
-    auth_dict = get_auth_token(auth)
+    auth_dict = get_auth_token(token)
 
     resp = JSON.parse(
         String(
-            HTTP.get("https://api.mybitx.com/api/1/balance";
+            HTTP.get(API_URL * "balance";
                 headers=auth_dict).body))
 
     BTC_bal = resp["balance"][2]
     ZAR_bal = resp["balance"][3]
-    BTC_Bal["amount"] = parse(Float64,BTC_Bal["balance"])
-    BTC_Bal["rand_value"] = BTC_Bal["btc_am"] * price["Last Trade"]
-    Zar_Bal["amount"] = parse(Float64,Zar_Bal["balance"])
-    Zar_Bal["btc_value"] = Zar_Bal["rand_val"]/price["Last Trade"]
-    return BTC_Bal, ZAR_bal, price
+
+    ZAR = Dict()
+    BTC = Dict()
+    BTC["amount"] = parse(Float64,BTC_bal["balance"])
+    BTC["rand_value"] = BTC["amount"] * price_dict["last_trade"]
+    ZAR["amount"] = parse(Float64,ZAR_bal["balance"])
+    ZAR["btc_value"] = ZAR["amount"]/price_dict["last_trade"]
+    return BTC, ZAR, price_dict
 end
 
-function stop_all_orders(auth=true)
-    auth_dict = get_auth_token(auth)
+function stop_all_orders(token="")
+    auth_dict = get_auth_token(token)
     query_dict = Dict("pair" => "XBTZAR", "state" => "PENDING")
     resp = JSON.parse(
         String(
-            HTTP.get("https://api.mybitx.com/api/1/listorders" ;
-                headers = header, query = query_dict).body))
+            HTTP.get(API_URL * "listorders" ;
+                headers = auth_dict, query = query_dict).body))
+    if resp["orders"] != nothing
+        for order in resp["orders"]
+            orderID = Dict("order_id" => order["order_id"])
+            HTTP.post(API_URL * "stoporder";
+                headers = auth_dict, query = orderID)
+            
+            order_id = orderID["order_id"]
+            pair = order["pair"]
+            volume = order["limit_volume"]
+            price = order["limit_price"]
+            type = order["type"]
 
-    for order in resp["orders"]
-        orderID = Dict("order_id" => order["order_id"])
-        HTTP.post("https://api.mybitx.com/api/1/stoporder";
-            headers = auth_dict, query = orderID)
+            println("$type limit order $order_id cancelled for pair $pair at price $price for volume $volume")
+        end
+    else
+        println("No orders pending")
     end
 end
+
+stop_all_orders()
