be34a04259195d8fce54bba2b3c3aaf8dbc07880
Michael Gant <michael.j.g.za@gmail.com>
2019-07-28 15:57:13 +0200

    export new functions

--- src/LunoAPI.jl
+++ src/LunoAPI.jl
@@ -4,7 +4,8 @@ using HTTP, Base64, JSON
 
 include("api.jl")
 
-export set_auth_token, get_auth_token, get_price, get_balance, stop_all_orders
+export set_auth_token, get_auth_token, get_price, get_balance, stop_all_orders,
+    execute_market_order, post_limit_order
 
 
 
